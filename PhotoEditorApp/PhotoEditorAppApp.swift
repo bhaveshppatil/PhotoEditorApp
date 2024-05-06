f38c649608559395b22df804a060b6dafbe8a289
Bhavesh Patil <bhavesh.patil0325@gmail.com>
2024-05-06 15:40:12 +0530

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by Bhavesh Patil on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
